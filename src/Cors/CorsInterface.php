1e146d9813ca519dbd90705b8e7ae685ad36760d
Thiago Paes <mrprompt@gmail.com>
2015-09-17 19:49:02 -0300

    init

---
+++ src/Cors/CorsInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of Enfants system
+ *
+ * @copyright Enfants
+ * @license   proprietary
+ */
+namespace MrPrompt\Silex\Cors;
+
+/**
+ * CORS validation service interface
+ *
+ * @author Thiago Paes <mrprompt@gmail.com>
+ * @author Marcel Araujo <admin@marcelaraujo.me>
+ */
+interface CorsInterface
+{
+    /**
+     * @var string
+     */
+    const HTTP_CORS = 'http.cors.service';
+}
---
+++ tests/Cors/CorsTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is a part of mrprompt/silex-di-container-provider.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license   MIT
+ */
+namespace MrPrompt\Tests\Silex\Cors;
+
+use MrPrompt\Silex\Cors\Cors;
+use PHPUnit_Framework_TestCase;
+use Silex\Application;
+
+/**
+ * @author Thiago Paes <mrprompt@gmail.com>
+ */
+class CorsTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Application
+     */
+    private $app;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app = new Application();
+    }
+
+    public function tearDown()
+    {
+        $this->app = null;
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test registration
+     *
+     * @test
+     */
+    public function registerMustBeCreateResources()
+    {
+        $this->app->register(new Cors());
+
+        $this->assertArrayHasKey(Cors::HTTP_CORS, $this->app);
+    }
+}
