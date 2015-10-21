4e1a451da53506b250a6fb5d9ac7844e211a8dfc
Thiago Paes <mrprompt@gmail.com>
2015-10-21 13:50:02 -0200

    Update CorsTest.php

--- tests/Cors/CorsTest.php
+++ tests/Cors/CorsTest.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * This file is a part of mrprompt/silex-di-container-provider.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @license   MIT
- */
 namespace MrPrompt\Tests\Silex\Cors;
 
 use MrPrompt\Silex\Cors\Cors;
